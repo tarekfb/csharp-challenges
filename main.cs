bfdf02ef3a58ade67f67380a8d8a4bf5f76ee301
tarekfb <56593098+tarekfb@users.noreply.github.com>
2025-02-25 11:51:35 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("C# Beginner Challenges Initialized!");
+        Console.WriteLine("Start solving challenges by editing this file.");
+    }
+}
